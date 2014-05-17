55827b681ff573f0803db7f2c7ff8ee9db880832
Syeryoha <syeryoha@yandex.ua>
2014-05-17 15:42:08 +0300

    Ruby fun

--- spec/requests/static_pages1s_spec.rb
+++ spec/requests/static_pages1s_spec.rb
@@ -9,10 +9,17 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 	
+	it "should not have the base tittle" do
+		visit '/static_pages/home'
+		expect(page).not_to have_title (' | Home')
+	end
+	
 	it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
+	
+	
   end
   
   
